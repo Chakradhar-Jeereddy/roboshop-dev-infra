6cb0c91242476e1e753093b78eee826a60d1d5f8
chakradhar06 <chakradhar06@gmail.com>
2025-10-26 18:40:47 -0400

    u

---
+++ 10-sg/locals.tf
@@ -0,0 +1,7 @@
+locals {
+    common_tags = {
+        project = var.project_name
+        env = var.environment
+    }
+    common_name_suffix = "{var.project_name}-${var.environment}" #roboshop-dev
+}
---
+++ 10-sg/main.tf
@@ -0,0 +1,9 @@
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "${local.common_name_suffix}-catalogue"
+  description = "Security group for catalogue with custom ports open within VPC, egress all trafic"
+  vpc_id      = "vpc-12345678"
+
+ 
+}
