66f0c188bea065664714000c45dcc1af0b01c6fc
chakradhar06 <chakradhar06@gmail.com>
2025-10-26 17:36:34 -0400

    u

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "remote-state-dev"
+    bucket = "s3-chakra-271"
     region = "us-east-1"
     key = "roboshop-dev-vpc"
     use_lockfile = true
