2edcff7dc4161645874927d0f324204c13648fd6
chakradhar06 <chakradhar06@gmail.com>
2025-10-26 20:58:22 -0400

    u

--- custom-sg/locals.tf
+++ custom-sg/locals.tf
@@ -6,11 +6,3 @@ locals {
     }
     common_name_suffix = "${var.project_name}-${var.environment}"
 }
-
-locals {
-    common_tags = {
-        Project = var.project_name
-        Environment = var.environment
-    }
-    common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
-}
