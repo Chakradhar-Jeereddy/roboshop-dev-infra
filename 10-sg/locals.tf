0f3e1f457541e9ce51d3cc2e1d4a6d033bea0ebb
chakradhar06 <chakradhar06@gmail.com>
2025-10-26 21:38:28 -0400

    u

--- 10-sg/locals.tf
+++ 10-sg/locals.tf
@@ -1,8 +1,4 @@
 locals {
-    common_tags = {
-        project = var.project_name
-        env = var.environment
-    }
     common_name_suffix = "${var.project_name}-${var.environment}" #roboshop-dev
     vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
