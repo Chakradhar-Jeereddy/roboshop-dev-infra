1c3215bf04e7e8b34ad014b5a563b2210904e943
chakradhar06 <chakradhar06@gmail.com>
2025-10-28 00:33:50 -0400

    u

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,12 +1,6 @@
 data "aws_ami" "ami" {
   executable_users = ["self"]
-
   filter {
-    name   = "source-image-region"
-    values = ["us-east-1"]
-  }
-
-   filter {
     name   = "image-id"
     values = ["ami-09c813fb71547fc4f"]
   }
