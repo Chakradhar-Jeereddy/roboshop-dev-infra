b887f8280dfdcb17df09445b00bb674759f37a8b
chakradhar06 <chakradhar06@gmail.com>
2025-10-28 01:40:45 -0400

    u

--- 20-bastion/locals.tf
+++ 20-bastion/locals.tf
@@ -1,6 +1,7 @@
 locals {
     ami_id = data.aws_ami.ami.id
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id
+    public_subnet_id = split(",",data.aws_ssm_parameter.public_subnet_id.value)[0]
     common_tags = {
         Project = var.project_name
         Environment = var.environment
