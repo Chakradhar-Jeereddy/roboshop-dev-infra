346ea1caf833790eeb531f40641facf760e6637f
chakradhar06 <chakradhar06@gmail.com>
2025-11-11 15:44:12 -0500

    u

--- 50-backen-alb/data.tf
+++ 50-backen-alb/data.tf
@@ -9,3 +9,7 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
 data "aws_ssm_parameter" "public_subnet_id" {
       name = "/${var.project_name}/${var.environment}/public_subnet_id"
 }
+
+data "aws_ssm_parameter" "private_subnet_id" {
+      name = "/${var.project_name}/${var.environment}/private_subnet_id"
+}
