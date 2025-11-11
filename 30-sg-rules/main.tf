e0305291022581fb1e3231dd93ddda55741b787d
chakradhar06 <chakradhar06@gmail.com>
2025-11-11 15:12:17 -0500

    u

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -7,3 +7,23 @@ resource "aws_security_group_rule" "frontend_frontend_alb" {
   protocol          = "tcp"
   to_port           = 80
 }
+
+#Rule for backend alb accepting frontend
+resource "aws_security_group_rule" "frontend_frontend_alb" {
+  type              = "ingress"
+  security_group_id = local.backend_alb_sg_id
+  source_security_group_id = local.frontend_sg_id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
+
+#Rule for backend alb accepting bastion
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  security_group_id = local.backend_alb_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
