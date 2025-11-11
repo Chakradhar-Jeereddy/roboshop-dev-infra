543beb92f664feb4c6b8ae17d7ef1f313e969c98
chakradhar06 <chakradhar06@gmail.com>
2025-11-11 15:21:21 -0500

    u

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -27,3 +27,13 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   protocol          = "tcp"
   to_port           = 80
 }
+
+#Rule for bastion accepting laptop
+resource "aws_security_group_rule" "bastion_laptop" {
+  type              = "ingress"
+  security_group_id = local.bastion_sg_id
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
