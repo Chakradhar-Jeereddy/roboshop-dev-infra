a855ba344ae80c0ae2fa89759b8c43a7100d3a6b
chakradhar06 <chakradhar06@gmail.com>
2025-11-12 02:02:44 -0500

    u

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,10 +4,17 @@ resource "aws_instance" "terraform" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id.value]
     user_data = file("bastion.sh")
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     tags = merge(
        local.common_tags,
        {
          Name = "${var.project_name}-${var.environment}-bastion"
        }
     )
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "bastion"
+  role = "BastionTerraformAdmin"
+  
 }
