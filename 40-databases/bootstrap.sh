6c2d2544f17533f460b98cd1d5e6835f783ea87d
chakradhar06 <chakradhar06@gmail.com>
2025-11-11 21:46:39 -0500

    u

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/Chakradhar-Jeereddy/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+#ansible-pull -U https://github.com/Chakradhar-Jeereddy/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+REPO_URL=https://github.com/Chakradhar-Jeereddy/ansible-roboshop-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-tf
+component=$1
+
+mkdir -p $REPO_URL
+mkdir -p /var/log/roboshop
+cd /var/log/roboshop
+touch ansible.log
+
+cd $REPO_DIR
+
+# check if ansible repo is already cloned or not
+if [ -d $ANSIBLE_DIR ]; then
+  cd $ANSIBLE_DIR
+  git pull
+else 
+  git clone $REPO_URL
+  cd $ANSIBLE_DIR
+fi
+
+ansible-playbook main.yaml -e component=$component
