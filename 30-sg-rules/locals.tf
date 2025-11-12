919c25328925471eef9cfc7c916d406578fd6d8e
chakradhar06 <chakradhar06@gmail.com>
2025-11-11 19:06:46 -0500

    u

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -3,4 +3,5 @@ locals {
     frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
     frontend_sg_id = data.aws_ssm_parameter.frontend_sg_id.value
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mongodb_sg_id= data.aws_ssm_parameter.mongodb_sg_id.value
 }
