b387947f1762043fcbf38870b6f045a2c8a2f9c3
chakradhar06 <chakradhar06@gmail.com>
2025-11-11 19:16:38 -0500

    u

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "mongodb" {
     tags = merge(
        local.common_tags,
        {
-         Name = "${local.common_name.suffix}-mongodb" #roboshop-dev-mongodb
+         Name = "${local.common_name_suffix}-mongodb" #roboshop-dev-mongodb
        }
     )
 }
