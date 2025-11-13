4a9e2bc99a525ade6983b64a5785078803726168
chakradhar06 <chakradhar06@gmail.com>
2025-11-12 22:56:30 -0500

    u

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -174,6 +174,7 @@ resource "aws_route53_record" "redis" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "rabbitmq" {
@@ -182,6 +183,7 @@ resource "aws_route53_record" "rabbitmq" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.rabbitmq.private_ip]
+  allow_overwrite = true
 }
 
 resource "aws_route53_record" "mysql" {
@@ -190,4 +192,5 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.mysql.private_ip]
+  allow_overwrite = true
 }
