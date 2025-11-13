c57e075198d7f9c627712d175d95f6e31923b572
chakradhar06 <chakradhar06@gmail.com>
2025-11-12 22:42:40 -0500

    u

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -159,3 +159,35 @@ provisioner "file" {
     ]
   }
 }
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = var.zone_id
+  name    = "mongodb-${var.environment}-${var.domain_name}" #mongodb-dev-chakra86.shop
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}-${var.domain_name}" #redis-dev-chakra86.shop
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = var.zone_id
+  name    = "rabbitmq-${var.environment}-${var.domain_name}" #rabitmq-dev-chakra86.shop
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = var.zone_id
+  name    = "mysql-${var.environment}-${var.domain_name}" #mysql-dev-chakra86.shop
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.mysql.private_ip]
+}
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z01570372XTUEJNI70A9H"
+}
+
+variable "domain_name" {
+    default = "chakra86.shop"
+}
