17e4d2627e55c42ab9befcc9aa6cd2fe20bbb16a
chakradhar06 <chakradhar06@gmail.com>
2025-11-14 18:34:17 -0500

    u

--- 50-backen-alb/main.tf
+++ 50-backen-alb/main.tf
@@ -37,8 +37,9 @@ resource "aws_route53_record" "backend_alb" {
   type    = "A"
 
   alias {
+    #These are alb details not our domain details
     name                   = aws_lb.backend_alb.dns_name
-    zone_id                = var.zone_id
+    zone_id                = aws_lb.backend_alb.zone_id
     evaluate_target_health = true
   }
 }
