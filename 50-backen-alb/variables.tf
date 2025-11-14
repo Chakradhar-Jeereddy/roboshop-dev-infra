53137866d4896242c18bed23dc751ac96a7d9b44
chakradhar06 <chakradhar06@gmail.com>
2025-11-14 18:27:48 -0500

    u

--- 50-backen-alb/variables.tf
+++ 50-backen-alb/variables.tf
@@ -7,3 +7,12 @@ variable "environment" {
     type = string
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z01570372XTUEJNI70A9H"
+}
+
+variable "domain_name" {
+    default = "chakra86.shop"
+  
+}
