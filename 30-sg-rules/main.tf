b1d3d44cde5a39fec8b712fbe87e83147743720a
chakradhar06 <chakradhar06@gmail.com>
2025-11-15 17:56:43 -0500

    u

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -108,11 +108,11 @@ resource "aws_security_group_rule" "catalogue_backen-alb" {
   to_port           = 8080
 }
 
-resource "aws_security_group_rule" "frontend-alb_user" {
+resource "aws_security_group_rule" "frontend-alb_public" {
   type              = "ingress"
   security_group_id = local.frontend_alb_sg_id
   cidr_blocks = [ "0.0.0.0/0" ]
   from_port         = 443
   protocol          = "tcp"
-  to_port           = 80
+  to_port           = 443
 } 
