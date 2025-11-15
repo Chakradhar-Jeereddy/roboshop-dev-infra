ae751e267996e833e2c31e28934f9cd896a07580
chakradhar06 <chakradhar06@gmail.com>
2025-11-15 17:19:05 -0500

    u

---
+++ 80-frontend-alb/main.tf
@@ -0,0 +1,51 @@
+# Create frontend application load balancer
+# Create alb listener
+# Create route53 recrod for ALB and alias
+
+resource "aws_lb" "frontend_alb" {
+  name               = "${local.common_name_suffix}-frontend-alb" #roboshop-dev-frontend-alb
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.frontend_alb_sg_id]
+  subnets            = local.public_subnet_ids
+
+  #enable_deletion_protection = false #Prevents accidential deletions from UI
+
+  tags = merge(
+         local.common_tags,
+         {
+          Name = "${local.common_name_suffix}-frontend-alb"
+         }
+  )
+}
+resource "aws_lb_listener" "frontend_alb" {
+  load_balancer_arn = aws_lb.frontend_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-TLS13-1-3-2021-06"
+  certificate_arn = local.frontend_alb_certificate_arn
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Hi, I am from frontend ALB HTTP"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "frontend_alb" {
+  zone_id = var.zone_id
+  name    = "*.frontend-alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    #These are alb details not our domain details
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,18 @@
+variable "project_name" {
+    type = string
+    default = "roboshop"
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01570372XTUEJNI70A9H"
+}
+
+variable "domain_name" {
+    default = "chakra86.shop"
+  
+}
