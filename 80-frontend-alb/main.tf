f43df7436e85ec9fe5c944427cfdc6e47d62fc0c
chakradhar06 <chakradhar06@gmail.com>
2025-11-15 18:27:11 -0500

    u

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -4,7 +4,7 @@
 
 resource "aws_lb" "frontend_alb" {
   name               = "${local.common_name_suffix}-frontend-alb" #roboshop-dev-frontend-alb
-  internal           = true
+  internal           = false
   load_balancer_type = "application"
   security_groups    = [local.frontend_alb_sg_id]
   subnets            = local.public_subnet_ids
@@ -21,27 +21,27 @@ resource "aws_lb_listener" "frontend_alb" {
   load_balancer_arn = aws_lb.frontend_alb.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy = "ELBSecurityPolicy-TLS13-1-3-2021-06"
-  certificate_arn = local.frontend_alb_certificate_arn
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-3-2021-06"
+  certificate_arn   = local.frontend_alb_certificate_arn
 
   default_action {
     type = "fixed-response"
 
     fixed_response {
-      content_type = "text/plain"
-      message_body = "<h1>Hi, I am from frontend ALB HTTP</h1>"
+      content_type = "text/html"
+      message_body = "<h1>Hi, I am from HTTPS frontend ALB</h1>"
       status_code  = "200"
     }
   }
 }
-
 resource "aws_route53_record" "frontend_alb" {
   zone_id = var.zone_id
-  name    = "roboshop-${var.environment}.${var.domain_name}" #roboshop.dev.chakra86.shop
+  name    = "roboshop-${var.environment}.${var.domain_name}" # roboshop-dev.chakra86.shop
   type    = "A"
   allow_overwrite = true
+
   alias {
-    #These are alb details not our domain details
+    # These are ALB details, not our domain details
     name                   = aws_lb.frontend_alb.dns_name
     zone_id                = aws_lb.frontend_alb.zone_id
     evaluate_target_health = true
