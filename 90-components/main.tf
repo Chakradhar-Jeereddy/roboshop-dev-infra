fbdef62d1fd1fe0e3208239e3d6182807a362fce
chakradhar06 <chakradhar06@gmail.com>
2025-11-16 06:49:34 -0500

    u

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,14 @@
-module "components" {
+/* module "components" {
+    for_each = var.components
     source = "../../terraform-roboshop-component"
     component = var.component
-    rule_priority = var.rule_priority
+    rule_priority = 10
+} */
+
+module "components" {
+    for_each = var.components
+    source = "git::https://github.com/Chakradhar-Jeereddy/terraform-roboshop-component.git?ref=main"
+    component = each.key
+    rule_priority = each.value.rule_priority
 }
 
--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -1,7 +1,30 @@
 variable "component" {
-    default = "catalogue"
+  default = "catalogue"
 }
-
 variable "rule_priority" {
     default = 10
+}
+
+variable "components" {
+    type = map
+    default = {
+        rule_shipping = {
+            priority = 40
+        }
+        catalogue = {
+            rule_priority = 10
+        }
+        cart = {
+            rule_priority = 30
+        }
+        payment = {
+            rule_priority = 50
+        }
+        user = {
+            rule_priority = 20
+        }
+        frontend = {
+            rule_priority = 10
+        }
+    }
 }
