1aa6df3c49f564c680a06941173a3444cb3d6cd2
chakradhar06 <chakradhar06@gmail.com>
2025-11-15 23:44:34 -0500

    u

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -8,8 +8,8 @@ locals {
     rabitmq_sg_id= data.aws_ssm_parameter.rabitmq_sg_id.value
     mysql_sg_id= data.aws_ssm_parameter.mysql_sg_id.value
     catalogue_sg_id= data.aws_ssm_parameter.catalogue_sg_id.value
-    user_sg_id = data.aws_ssm_parameter.user_sg_id
-    shipping_sg_id = data.aws_ssm_parameter.shipping_sg_id
-    cart_sg_id = data.aws_ssm_parameter.cart_sg_id
-    payment_sg_id = data.aws_ssm_parameter.payment_sg_id
+    user_sg_id = data.aws_ssm_parameter.user_sg_id.value
+    shipping_sg_id = data.aws_ssm_parameter.shipping_sg_id.value
+    cart_sg_id = data.aws_ssm_parameter.cart_sg_id.value
+    payment_sg_id = data.aws_ssm_parameter.payment_sg_id.value
 }
